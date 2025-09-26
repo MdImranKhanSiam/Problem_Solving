908e6ca4cd4e822e2ca3147b67534b5f76908fd0
Imran Khan <mdimrankhansiam@gmail.com>
2025-09-26 13:57:15 +0600

    Progress

---
+++ Backend/NodeJS/Practice/project_1/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { readFile } = require('fs');
+
+const app = express();
+
+app.get('/', async (request,response) => {
+
+    response.send( await readFile('home.html', 'utf-8') );
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('App on http://localhost:3000'))
