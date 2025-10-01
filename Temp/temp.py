883e02dc32cf461c8a2c466bf9a4e2ec23fdfc1b
Imran Khan <mdimrankhansiam@gmail.com>
2025-10-02 02:11:53 +0600

    Progress

--- Temp/temp.py
+++ Temp/temp.py
@@ -1,7 +1,41 @@
-n = int(input())
+def recursion(a,b,c,d):
+    global total_recursion
+    global min_step
 
-for i in range(1,n+1):
-    for j in range(0,i):
-        print('*',end="")
+    total_recursion += 1
+    
+    if total_recursion > 1000:
+        return
 
-    print()
+    N = a
+    D = b
+    X = c
+    Step = d
+    
+    # print(Step)
+
+    if X > N:
+        # print('Yeah')
+        return
+
+    if X == N:
+        if Step < min_step:
+            min_step = Step
+
+    recursion(N,D,int(X*2),Step+1)
+    recursion(N,D,int(X+D),Step+1)
+
+tc = int(input())
+
+for i in range(0,tc):
+    global total_recursion
+    global min_step
+    limit = int(1e18)+9
+    total_recursion = 0
+    min_step = limit
+    n, d = list(map(int,input().split()))
+    recursion(n,d,1,0)
+    if min_step == limit:
+        print(-1)
+    else:
+        print(f'{min_step}')
